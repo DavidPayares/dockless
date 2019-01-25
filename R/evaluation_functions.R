54facdef55cc17776fc2e4f2339d05f154dcf695
luukvdmeer <luukvandermeer@live.nl>
2019-01-25 12:47:24 +0100

    Added RMSE as error metric

--- R/evaluation_functions.R
+++ R/evaluation_functions.R
@@ -1,29 +1,45 @@
-#' Mean absolute error
+#' Forecast error
 #'
-#' Calculates the mean absolute error of one or more forecasts.
+#' Calculates either the root mean squared error or the mean absolute
+#' error of a set of forecasts.
 #'
 #' @param x object either of class \code{dockless_fc} or \code{dockless_fcc}.
+#' @param type one of 'RMSE' or 'MAE'.
 #' @return Returns a numeric value.
 #' @export
-mae = function(x) UseMethod("mae")
+error = function(x, type) UseMethod("error")
 
 
-#' @name mae
+#' @name error
 #' @export
-mae.dockless_fc = function(x) {
+error.dockless_fc = function(x, type) {
 
-  # Calculate mean absolute error
-  mean(abs(x$error), na.rm = TRUE)
+  if (type = 'RMSE') {
+
+    # Calculate root mean squared error
+    sqrt(mean((x$error) * (x$error), na.rm = TRUE))
+
+  } else if (type = 'MAE') {
+
+    # Calculate mean absolute error
+    mean(abs(x$error), na.rm = TRUE)
+
+  } else {
+
+    # Stop the function
+    stop("Type should be either 'RMSE' or 'MAE'")
+
+  }
 
 }
 
 
-#' @name mae
+#' @name error
 #' @export
-mae.dockless_fcc = function(x) {
+error.dockless_fcc = function(x, type) {
 
-  # Calculate mae per dockless_fc
-  errors = sapply(x, function(x) mae(x))
+  # Calculate error per dockless_fc
+  errors = sapply(x, function(x) error(x, type = type))
 
   # Average
   mean(errors, na.rm = TRUE)
@@ -31,40 +47,62 @@ mae.dockless_fcc = function(x) {
 }
 
 
-#' Mean absolute error per hour of the day
+#' Forecast error per hour of the day
 #'
-#' Calculates the mean absolute error per hour of the day.
+#' Calculates eiter the root mean squared error or the mean absolute
+#' error, per hour of the day.
 #'
 #' @param x object either of class \code{dockless_fc} or \code{dockless_fcc}..
+#' @param type one of 'RMSE' or 'MAE'.
 #' @return Returns a vector of numeric values.
 #' @export
-mae_hourofday = function(x) UseMethod("mae_hourofday")
+error_hourofday = function(x, type) UseMethod("error_hourofday")
 
 
-#' @name mae_hourofday
+#' @name error_hourofday
 #' @export
-mae_hourofday.dockless_fc = function(x) {
-
-  # Convert error into absolute error
-  x$error = abs(x$error)
+error_hourofday.dockless_fc = function(x, type) {
 
   # Add a column specifying the hour of the day
   x$hour = lubridate::hour(x$time)
 
-  # Aggregate by hour and calculate mae
-  agg = stats::aggregate(x$error, by = list(x$hour), FUN = mean)
+  if (type = 'RMSE') {
 
-  # Return the mae's as named vector
+    # Convert error into squared error
+    x$error = (x$error)^2
+
+    # Aggregate by hour and calculate mse
+    agg = stats::aggregate(x$error, by = list(x$hour), FUN = mean)
+
+    # Take square root
+    agg[, 2] = sqrt(agg[, 2])
+
+  } else if (type = 'MAE') {
+
+    # Convert error into absolute error
+    x$error = abs(x$error)
+
+    # Aggregate by hour and calculate mae
+    agg = stats::aggregate(x$error, by = list(x$hour), FUN = mean)
+
+  } else {
+
+    # Stop the function
+    stop("Type should be either 'RMSE' or 'MAE'")
+
+  }
+
+  # Return the errors as named vector
   stats::setNames(agg[,2], nm = agg[,1])
 
 }
 
-#' @name mae_hourofday
+#' @name error_hourofday
 #' @export
-mae_hourofday.dockless_fcc = function(x) {
+error_hourofday.dockless_fcc = function(x, type) {
 
-  # Calculate mae per hour of day for each dockless_fc
-  errors = sapply(x, function(x) mae_hourofday(x))
+  # Calculate error per hour of day for each dockless_fc
+  errors = sapply(x, function(x) error_hourofday(x, type = type))
 
   # Average per hour of day
   rowMeans(errors, na.rm = TRUE)
@@ -72,34 +110,56 @@ mae_hourofday.dockless_fcc = function(x) {
 }
 
 
-#' Mean absolute error per forecast lag
+#' Forecast error per forecast lag
 #'
-#' Calculates the mean absolute error per forecast lag.
+#' Calculates either the root mean squared error or the mean absolute
+#' error, per forecast lag.
 #'
 #' @param x object either of class \code{dockless_fc} or \code{dockless_fcc}..
+#' @param type one of 'RMSE' or 'MAE'.
 #' @return Returns a vector of numeric values.
 #' @export
-mae_lag = function(x) UseMethod("mae_lag")
+error_lag = function(x, type) UseMethod("error_lag")
 
 
-#' @name mae_lag
+#' @name error_lag
 #' @export
-mae_lag.dockless_fc = function(x) {
+error_lag.dockless_fc = function(x, type) {
 
-  # Convert error into absolute error
-  x$error = abs(x$error)
+  if (type = 'RMSE') {
 
-  # Return as named vector
-  stats::setNames(x$error, nm = seq(1, nrow(x), 1))
+    # Convert error into squared error
+    x$error = (x$error)^2
+
+    # Take square root
+    agg[, 2] = sqrt(agg[, 2])
+
+    # Return as named vector
+    stats::setNames(x$error, nm = seq(1, nrow(x), 1))
+
+  } else if (type = 'MAE') {
+
+    # Convert error into absolute error
+    x$error = abs(x$error)
+
+    # Return as named vector
+    stats::setNames(x$error, nm = seq(1, nrow(x), 1))
+
+  } else {
+
+    # Stop the function
+    stop("Type should be either 'RMSE' or 'MAE'")
+
+  }
 
 }
 
-#' @name mae_lag
+#' @name error_lag
 #' @export
-mae_lag.dockless_fcc = function(x) {
+error_lag.dockless_fcc = function(x, type) {
 
-  # Calculate mae per forecast lag for each dockless_fc
-  errors = sapply(x, function(x) mae_lag(x))
+  # Calculate error per forecast lag for each dockless_fc
+  errors = sapply(x, function(x) error_lag(x, type = type))
 
   # Average per hour of day
   rowMeans(errors, na.rm = TRUE)
