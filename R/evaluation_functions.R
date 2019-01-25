d51bcd6b7e81fa51ee4bb0e1837ad623c545e515
luukvdmeer <luukvandermeer@live.nl>
2019-01-25 13:20:49 +0100

    Debugged evaluation functions

--- R/evaluation_functions.R
+++ R/evaluation_functions.R
@@ -17,14 +17,14 @@ error = function(x, type, average = TRUE) UseMethod("error")
 
 #' @name error
 #' @export
-error.dockless_fc = function(x, type) {
+error.dockless_fc = function(x, type, average = TRUE) {
 
-  if (type = 'RMSE') {
+  if (type == 'RMSE') {
 
     # Calculate root mean squared error
     sqrt(mean((x$error) * (x$error), na.rm = TRUE))
 
-  } else if (type = 'MAE') {
+  } else if (type == 'MAE') {
 
     # Calculate mean absolute error
     mean(abs(x$error), na.rm = TRUE)
@@ -80,7 +80,7 @@ error_hourofday.dockless_fc = function(x, type) {
   # Add a column specifying the hour of the day
   x$hour = lubridate::hour(x$time)
 
-  if (type = 'RMSE') {
+  if (type == 'RMSE') {
 
     # Convert error into squared error
     x$error = (x$error)^2
@@ -91,7 +91,7 @@ error_hourofday.dockless_fc = function(x, type) {
     # Take square root
     agg[, 2] = sqrt(agg[, 2])
 
-  } else if (type = 'MAE') {
+  } else if (type == 'MAE') {
 
     # Convert error into absolute error
     x$error = abs(x$error)
@@ -129,7 +129,7 @@ error_hourofday.dockless_fcc = function(x, type) {
 #' Calculates either the root mean squared error or the mean absolute
 #' error, per forecast lag.
 #'
-#' @param x object either of class \code{dockless_fc} or \code{dockless_fcc}..
+#' @param x object either of class \code{dockless_fc} or \code{dockless_fcc}.
 #' @param type one of 'RMSE' or 'MAE'.
 #' @return Returns a vector of numeric values.
 #' @export
@@ -140,7 +140,7 @@ error_lag = function(x, type) UseMethod("error_lag")
 #' @export
 error_lag.dockless_fc = function(x, type) {
 
-  if (type = 'RMSE') {
+  if (type == 'RMSE') {
 
     # Convert error into squared error
     x$error = (x$error)^2
@@ -151,7 +151,7 @@ error_lag.dockless_fc = function(x, type) {
     # Return as named vector
     stats::setNames(x$error, nm = seq(1, nrow(x), 1))
 
-  } else if (type = 'MAE') {
+  } else if (type == 'MAE') {
 
     # Convert error into absolute error
     x$error = abs(x$error)
@@ -190,7 +190,7 @@ error_lag.dockless_fcc = function(x, type) {
 #' @param type one of 'RMSE' or 'MAE'.
 #' @return Returns a vector of numeric values.
 #' @export
-error_lag = function(x, clusters, type) {
+error_cluster = function(x, clusters, type) {
 
   # Calculate forecast error per dockless_fc
   errors_vec = error(x, type = type, average = FALSE)
