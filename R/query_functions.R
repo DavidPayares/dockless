5aaa5756123df2e477756403ff6bbf582d974883
luukvdmeer <luukvandermeer@live.nl>
2019-01-29 20:29:16 +0100

    Debugged query functions

--- R/query_functions.R
+++ R/query_functions.R
@@ -383,10 +383,7 @@ query_usage = function(bikes,
   # Disconnect to the database
   RPostgreSQL::dbDisconnect(db_connection)
 
-  # Remove NULL elements
-  all_data_cleaned = all_data[-which(sapply(all_data, is.null))]
-
   # Return as a single object
-  do.call('rbind', all_data_cleaned)
+  do.call('rbind', all_data)
 
 }
